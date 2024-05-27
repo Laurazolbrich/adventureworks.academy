44be12115e516dde2da37878fa7b368e0c0a8c01
Laura Olbrich <laura.zolbrich@gmail.com>
2024-05-27 17:35:19 +0000

    Finalizada as stages da dim_products e da dim_data

---
+++ models/staging/erp/stg_erp__sales_order_header.sql
@@ -0,0 +1,23 @@
+with
+    fonte_sales_order_header as (
+        select 
+            CAST(SALESORDERID AS INT) AS pk_salesorderid
+            , CAST(ORDERDATE AS DATE) AS orderdate
+            , CAST(DUEDATE AS DATE) AS duedate
+            , CAST(SHIPDATE AS DATE) AS shipdate
+            , CAST(STATUS AS INT) AS status
+            , CAST(SHIPTOADDRESSID AS INT) AS shiptoaddressid
+            , CAST(CREDITCARDID AS INT) AS creditcardid
+            , CAST(CREDITCARDAPPROVALCODE AS VARCHAR) AS creditcardapprovalcode
+            , CAST(CURRENCYRATEID AS INT) AS currencyrateid
+            , CAST(SUBTOTAL AS DECIMAL) AS subtotal
+            , CAST(TAXAMT AS DECIMAL) AS taxamt
+            , CAST(FREIGHT AS DECIMAL) AS freight
+            , CAST(TOTALDUE AS DECIMAL) AS totaldue
+            , CAST(ROWGUID AS VARCHAR(36)) AS rowguid_salesorderheader
+            , CAST(MODIFIEDDATE AS DATE) AS modifieddate_salesorderheader
+        from {{ source('ERP', 'SALESORDERHEADER') }}
+    )
+
+select *
+from fonte_sales_order_header
