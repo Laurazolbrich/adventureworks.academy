ce0f186450569e390b7de7f8cff808502c14042c
Laura Olbrich <laura.zolbrich@gmail.com>
2024-05-27 22:33:35 +0000

    Feito a dim_sales_reason

--- models/staging/erp/stg_erp__salesorderheadersalesreason.sql
+++ models/staging/erp/stg_erp__salesorderheadersalesreason.sql
@@ -1,8 +1,8 @@
 with
     fonte_salesorderheadersalesreason as (
         select 
-            cast(SALESORDERID as int) as pk_salesorderheadersalesreason
-            , cast(SALESREASONID as int) as fk_salesorderheadersalesreason
+            cast(SALESORDERID as int) as pk_salesorderid
+            , cast(SALESREASONID as int) as fk_salesreasonid
             , cast(MODIFIEDDATE as date) as modifieddate_salesorderheadersalesreason
         from {{ source('ERP', 'SALESORDERHEADERSALESREASON') }}
     )
